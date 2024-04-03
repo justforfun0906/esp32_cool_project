aa3cf896d3f60dd375c92b1e4ce2b479d641c4cd
justforfun0906 <jimmy0906abc@gmail.com>
2024-04-03 13:08:40 +0800

    first commit!
    yeah

---
+++ get_weather/get_weather.ino
@@ -0,0 +1,89 @@
+#include <WiFi.h>
+
+const char* ssid     = "310"; // Change this to your WiFi SSID
+const char* password = "88888888"; // Change this to your WiFi password
+
+string url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-053";
+string authorization = "CWB-4E884048-6F63-4D56-AA33-D37CD194C120";
+
+// The default example accepts one data filed named "field1"
+// For your own server you can ofcourse create more of them.
+int field1 = 0;
+
+int numberOfResults = 3; // Number of results to be read
+int fieldNumber = 1; // Field number which will be read out
+
+void setup()
+{
+    Serial.begin(115200);
+    while(!Serial){delay(100);}
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println("******************************************************");
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void readResponse(WiFiClient *client){
+  unsigned long timeout = millis();
+  while(client->available() == 0){
+    if(millis() - timeout > 5000){
+      Serial.println(">>> Client Timeout !");
+      client->stop();
+      return;
+    }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  while(client->available()) {
+    String line = client->readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.printf("\nClosing connection\n\n");
+}
+
+void loop(){
+  WiFiClient client;
+  String footer = String(" HTTP/1.1\r\n") + "Host: " + String(host) + "\r\n" + "Connection: close\r\n\r\n";
+
+  // WRITE --------------------------------------------------------------------------------------------
+  if (!client.connect(host, httpPort)) {
+    return;
+  }
+
+  client.print("GET /update?api_key=" + writeApiKey + "&field1=" + field1 + footer);
+  readResponse(&client);
+
+  // READ --------------------------------------------------------------------------------------------
+
+  String readRequest = "GET /channels/" + channelID + "/fields/" + fieldNumber + ".json?results=" + numberOfResults + " HTTP/1.1\r\n" +
+                       "Host: " + host + "\r\n" +
+                       "Connection: close\r\n\r\n";
+
+  if (!client.connect(host, httpPort)) {
+    return;
+  }
+
+  client.print(readRequest);
+  readResponse(&client);
+
+  // -------------------------------------------------------------------------------------------------
+
+  ++field1;
+  delay(10000);
+}
